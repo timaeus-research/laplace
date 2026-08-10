8ad1749667ef100714eea8492f427e3d1135fc0a
dmurfet <daniel.murfet@gmail.com>
2026-08-10 11:11:25 -0700

    Radial Taylor bound: package family from smoothness alone

--- Laplace.lean
+++ Laplace.lean
@@ -57,6 +57,7 @@ import Laplace.Multi.MultilinearDiagonal
 import Laplace.Multi.RateCalculus
 import Laplace.Multi.LocalRateDCT
 import Laplace.Multi.PackageConstructor
+import Laplace.Multi.RadialTaylorBound
 import Laplace.Multi.PairwiseRate
 import Laplace.Multi.NormalizedRate
 import Laplace.Multi.DegreeRecovery
